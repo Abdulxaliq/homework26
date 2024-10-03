8bc68142b66083c32103ac3bc134725508b1915f
Abdulxaliq <Agaye_ck17@itstep.edu.az>
2024-10-03 22:02:12 +0400

    Add project files.

---
+++ homework26/homework26.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <iomanip>  
+
+using namespace std;
+
+int main()
+{
+    
+    //const int size = 10;
+    //cout << "    ";
+    //for (int i = 1; i <= size; ++i) {
+    //    cout << setw(4) << i;
+    //}
+    //cout << endl;
+    //cout << "   +";
+    //for (int i = 1; i <= size; ++i) {
+    //    cout << "----";
+    //}
+    //cout << endl;
+    //for (int i = 1; i <= size; ++i) {
+    //    cout << setw(2) << i << " |";
+    //    for (int j = 1; j <= size; ++j) {
+    //        cout << setw(4) << (i * j);
+    //    }
+    //    cout << endl;
+    //}
+
+
+
+
+
+    //int side;
+    //cout << "Enter a side of the square: ";
+    //cin >> side;
+    //for (int i = 0; i < side; i++) {
+    //    for (int j = 0; j < side; j++) {
+    //        if (i == 0 || i == side - 1 || j == 0 || j == side - 1) {
+    //            cout << "*";
+    //        }
+    //        else if (i == side / 2 && (j > 0 && j < side - 1)) {
+    //            cout << " ";
+    //        }
+    //        else {
+    //            cout << "*";
+    //        }
+    //    }
+    //    cout << endl;
+    //}
+
+
+
+
+
+
+    //int height;
+    //cout << "Enter a height of the square: ";
+    //cin >> height;
+    //for (int i = 1; i <= height; i++) { 
+    //    for (int j = 1; j <= height - i; j++) {
+    //        cout << " ";
+    //    }
+    //    for (int k = 1; k <= (2 * i - 1); k++) {
+    //        cout << "*";
+    //    }
+    //    cout << endl;
+    //}
+
+
+
+
+
+
+    //int height;
+    //cout << "Enter a height of the square: ";
+    //cin >> height;  
+    //for (int i = 1; i <= height / 2; i++) { 
+    //    for (int j = 1; j <= height / 2 - i; j++) {
+    //        cout << " ";
+    //    } 
+    //    for (int k = 1; k <= 2 * i; k++) {
+    //        cout << "*";
+    //    }
+    //    cout << std::endl;
+    //}
+    //for (int i = height / 2; i >= 1; i--) {
+    //    for (int j = 1; j <= height / 2 - i; j++) {
+    //        cout << " ";
+    //    }
+    //    for (int k = 1; k <= 2 * i; k++) {
+    //        cout << "*";
+    //    }
+    //    cout << std::endl;
+    //}
+
+
+
+
+
+
+
+
+    //int height;
+    //cout << "Enter a height of the butterfly: ";
+    //cin >> height;
+    //for (int i = 1; i <= height; i++) {
+
+    //    for (int j = 1; j <= i; j++) {
+    //        cout << "*";
+    //    }
+    //    for (int j = 1; j <= (2 * (height - i)); j++) {
+    //        cout << " ";
+    //    }
+
+    //    for (int j = 1; j <= i; j++) {
+    //        cout << "*";
+    //    }
+    //    cout << endl;
+    //}
+    //for (int i = height; i >= 1; i--) {
+ 
+    //    for (int j = 1; j <= i; j++) {
+    //        cout << "*";
+    //    }
+    //    for (int j = 1; j <= (2 * (height - i)); j++) {
+    //        cout << " ";
+    //    } 
+    //    for (int j = 1; j <= i; j++) {
+    //        cout << "*";
+    //    }
+    //    cout << endl;
+    //}
+
+
+
+    //const int size = 8;
+    //for (int i = 0; i < size; i++) {
+    //    for (int j = 0; j < size; j++) {
+    //        if ((i + j) % 2 == 0) {
+    //            cout << char(219);
+    //        }
+    //        else {
+    //            cout << " ";
+    //        }
+    //    }
+    //    cout << endl;
+    //}
+}
+
+
